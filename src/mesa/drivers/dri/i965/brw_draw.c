2b2fb4aeeaed7bd54b12fa8e0d4493d6b87ce15f
Paul Berry <stereotype441@gmail.com>
2012-07-03 08:35:25 -0700

    HACK: disable normal drawing.

--- src/mesa/drivers/dri/i965/brw_draw.c
+++ src/mesa/drivers/dri/i965/brw_draw.c
@@ -428,6 +428,8 @@ static bool brw_try_draw_prims( struct gl_context *ctx,
     */
    brw_predraw_resolve_buffers(brw);
 
+   goto out; /* HACK */
+
    /* Bind all inputs, derive varying and size information:
     */
    brw_merge_inputs( brw, arrays );
