fdc794738ed2bd3f10568415fdcf7f48273a6704
Bryan Cain <bryancain3@gmail.com>
2012-07-26 13:56:06 -0500

    draw/gs: fix allocation of buffer for GS output vertices
    Before, it accounted for the size of the vertices but not the other fields
    in the vertex_header struct, which caused memory corruption.

--- src/gallium/auxiliary/draw/draw_gs.c
+++ src/gallium/auxiliary/draw/draw_gs.c
@@ -423,7 +423,8 @@ int draw_geometry_shader_run(struct draw_geometry_shader *shader,
    output_verts->vertex_size = vertex_size;
    output_verts->stride = output_verts->vertex_size;
    output_verts->verts =
-      (struct vertex_header *)MALLOC(output_verts->vertex_size *
+      (struct vertex_header *)MALLOC(sizeof(struct vertex_header) +
+                                     output_verts->vertex_size *
                                      num_in_primitives *
                                      shader->max_output_vertices);
 
