6aae061c8e444243a30abf4eeea605fbdae82493
Paul Berry <stereotype441@gmail.com>
2012-10-08 14:30:23 -0700

    First cut at code generator for marshal.c

---
+++ src/mapi/glapi/gen/gl_marshal.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2012 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+import license
+import gl_XML, glX_XML
+import sys, getopt
+
+header = """
+#include "marshal_generatable.h"
+
+#include "api_exec.h"
+#include "context.h"
+#include "dispatch.h"
+#include "marshal.h"
+"""
+
+footer = """/* FOOTER */
+"""
+
+
+class PrintCode(gl_XML.gl_print_base):
+
+	def __init__(self):
+		gl_XML.gl_print_base.__init__(self)
+
+		self.name = "gl_marshal.py (from Mesa)"
+		self.license = license.bsd_license_template % (
+                        "Copyright (C) 2012 Intel Corporation",
+                        "INTEL CORPORATION")
+
+		return
+
+
+	def get_stack_size(self, f):
+                raise Exception("Unneeded?")
+		size = 0
+		for p in f.parameterIterator():
+			if p.is_padding:
+				continue
+
+			size += p.get_stack_size()
+
+		return size
+
+
+	def printRealHeader(self):
+		print header
+		return
+
+
+	def printRealFooter(self):
+		print footer
+		return
+
+
+        def classify_function(self, f):
+                # TODO: should go into function class
+                if f.return_type != 'void':
+                        return 'sync'
+                if any([p.is_pointer() for p in f.parameters]):
+                        return 'sync'
+                return 'async'
+
+
+        def print_sync_body(self, func):
+                print '/* {0}: marshalled synchronously */'.format(func.name)
+                print 'static const {0} GLAPIENTRY'.format(func.return_type)
+                print 'marshal_{0}({1})'.format(
+                        func.name, func.get_parameter_string())
+                print '{'
+                print '   GET_CURRENT_CONTEXT(ctx);'
+                print '   _mesa_marshal_synchronize(ctx);'
+                call = 'CALL_{0}(ctx->CurrentServerDispatch, ({1}))'.format(
+                        func.name, func.get_called_parameter_string())
+                if func.return_type == 'void':
+                        print '   {0};'.format(call)
+                else:
+                        print '   return {0};'
+                print '}'
+                print ''
+                print ''
+
+
+        def print_async_body(self, func):
+                print '/* {0}: marshalled asynchronously */'.format(func.name)
+                print 'struct marshal_cmd_{0}'.format(func.name)
+                print '{'
+                print '   struct marshal_cmd_base cmd_base;'
+                params = [p for p in func.parameters if not p.is_padding]
+                for p in params:
+                        print '   {0} {1};'.format(p.type_string(), p.name)
+                print '};'
+                print 'static inline void'
+                print ('unmarshal_{0}(struct gl_context *ctx, '
+                       'const struct marshal_cmd_{0} *cmd)').format(func.name)
+                print '{'
+                print '   CALL_{0}(ctx->CurrentServerDispatch, ({1}));'.format(
+                        func.name,
+                        ', '.join('cmd->{0}'.format(p.name)
+                                  for p in func.parameters))
+                print '}'
+                print 'static void GLAPIENTRY'
+                print 'marshal_{0}({1})'.format(
+                        func.name, func.get_parameter_string())
+                print '{'
+                print '   GET_CURRENT_CONTEXT(ctx);'
+                struct = 'struct marshal_cmd_{0}'.format(func.name)
+                print '   {0} *cmd ='.format(struct)
+                print ('      _mesa_allocate_command_in_queue(ctx, '
+                       'DISPATCH_CMD_{0}, sizeof({1})').format(
+                        func.name, struct)
+                for p in params:
+                        print '   cmd->{0} = {0};'.format(p.name)
+                if not params:
+                        print '   (void) cmd;\n'
+                print '   _mesa_post_marshal_hook(ctx);'
+                print '}'
+                print ''
+                print ''
+
+
+        def print_unmarshal_dispatch_cmd(self, funcs):
+                print 'size_t'
+                print ('_mesa_unmarshal_dispatch_cmd(struct gl_context *ctx, '
+                       'const void *cmd)')
+                print '{'
+                print '   const struct marshal_cmd_base *cmd_base = cmd;'
+                print '   switch (cmd_base->cmd_id) {'
+                for func in funcs:
+                        print '   case DISPATCH_CMD_{0}:'.format(func.name)
+                        print ('      unmarshal_{0}(ctx, '
+                               '(const struct marshal_cmd_{0} *)'
+                               ' cmd);').format(func.name)
+                        print '      break;'
+                print '   default:'
+                print '      assert(!"Unrecognized command ID");'
+                print '      break;'
+                print '   }'
+                print ''
+                print '   return cmd_base->cmd_size;'
+                print '}'
+                print ''
+                print ''
+
+
+	def printBody(self, api):
+                async_funcs = []
+                for func in api.functionIterateAll():
+                        classification = self.classify_function(func)
+                        if classification == 'sync':
+                                self.print_sync_body(func)
+                        elif classification == 'async':
+                                self.print_async_body(func)
+                                async_funcs.append(func)
+                        else:
+                                raise Exception(
+                                        'Unexpected classification {0}'.format(
+                                                classification))
+                self.print_unmarshal_dispatch_cmd(async_funcs)
+
+
+def show_usage():
+	print "Usage: %s [-f input_file_name]" % sys.argv[0]
+	sys.exit(1)
+
+if __name__ == '__main__':
+	file_name = "gl_API.xml"
+
+	try:
+		(args, trail) = getopt.getopt(sys.argv[1:], "m:f:")
+	except Exception,e:
+		show_usage()
+
+	for (arg,val) in args:
+		if arg == "-f":
+			file_name = val
+
+	printer = PrintCode()
+
+	api = gl_XML.parse_GL_API(file_name, glX_XML.glx_item_factory())
+	printer.Print(api)
