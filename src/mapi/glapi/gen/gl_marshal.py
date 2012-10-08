b580e99931ca0d7cb4b2feea8bd758091617795f
Paul Berry <stereotype441@gmail.com>
2012-10-08 16:24:25 -0700

    Implement simple variable parameters

--- src/mapi/glapi/gen/gl_marshal.py
+++ src/mapi/glapi/gen/gl_marshal.py
@@ -22,7 +22,7 @@
 # IN THE SOFTWARE.
 
 import license
-import gl_XML, glX_XML
+import gl_XML
 import sys, getopt
 
 header = """
@@ -84,47 +84,132 @@ class PrintCode(gl_XML.gl_print_base):
                         return 'sync'
                 if f.return_type != 'void':
                         return 'sync'
-                if any([p.is_pointer() for p in f.parameters]):
-                        return 'sync'
+                for p in f.parameters:
+                        if p.is_output:
+                                return 'sync'
+                        if p.is_pointer() and not (p.count or p.counter):
+                                return 'sync'
+                        if p.count_parameter_list:
+                                # Parameter size is determined by
+                                # enums; don't know how to do this
+                                # yet.
+                                return 'sync'
                 return 'async'
+
+
+        def print_sync_call(self, func, indent):
+                call = 'CALL_{0}(ctx->CurrentServerDispatch, ({1}))'.format(
+                        func.name, func.get_called_parameter_string())
+                if func.return_type == 'void':
+                        print indent + '{0};'.format(call)
+                else:
+                        print indent + 'return {0};'
+
+
+        def print_sync_dispatch(self, func, indent):
+                print indent + '_mesa_marshal_synchronize(ctx);'
+                self.print_sync_call(func, indent)
 
 
         def print_sync_body(self, func):
                 print '/* {0}: marshalled synchronously */'.format(func.name)
-                print 'static const {0} GLAPIENTRY'.format(func.return_type)
+                print 'static {0} GLAPIENTRY'.format(func.return_type)
                 print 'marshal_{0}({1})'.format(
                         func.name, func.get_parameter_string())
                 print '{'
                 print '   GET_CURRENT_CONTEXT(ctx);'
-                print '   _mesa_marshal_synchronize(ctx);'
-                call = 'CALL_{0}(ctx->CurrentServerDispatch, ({1}))'.format(
-                        func.name, func.get_called_parameter_string())
-                if func.return_type == 'void':
-                        print '   {0};'.format(call)
-                else:
-                        print '   return {0};'
+                self.print_sync_call(func, '   ')
                 print '}'
                 print ''
                 print ''
 
 
+        def print_async_dispatch(self, func, struct, fixed_params,
+                                 variable_params, indent):
+                def pr(s):
+                        print indent + s
+                pr('{0} *cmd ='.format(struct))
+                pr(('   _mesa_allocate_command_in_queue(ctx, '
+                    'DISPATCH_CMD_{0}, cmd_size);').format(func.name))
+                for p in fixed_params:
+                        pr('cmd->{0} = {0};'.format(p.name))
+                if variable_params:
+                        pr('char *variable_data = (char *) (cmd + 1);')
+                        for p in variable_params:
+                                pr(('memcpy(variable_data, '
+                                    '{0}, {1});').format(
+                                                p.name, p.size_string(False)))
+                                pr('variable_data += {0};'.format(
+                                                p.size_string(False)))
+                if not fixed_params and not variable_params:
+                        pr('(void) cmd;\n')
+                pr('_mesa_post_marshal_hook(ctx);')
+
+
         def print_async_body(self, func):
+                fixed_params = []
+                variable_params = []
+                for p in func.parameters:
+                        if p.is_padding:
+                                continue
+                        if p.is_variable_length():
+                                variable_params.append(p)
+                        else:
+                                fixed_params.append(p)
                 print '/* {0}: marshalled asynchronously */'.format(func.name)
                 print 'struct marshal_cmd_{0}'.format(func.name)
                 print '{'
                 print '   struct marshal_cmd_base cmd_base;'
-                params = [p for p in func.parameters if not p.is_padding]
-                for p in params:
-                        print '   {0} {1};'.format(p.type_string(), p.name)
+                for p in fixed_params:
+                        if p.count:
+                                print '   {0} {1}[{2}];'.format(
+                                        p.get_base_type_string(),
+                                        p.name, p.count)
+                        else:
+                                print '   {0} {1};'.format(
+                                        p.type_string(), p.name)
+                # TODO: when there are variable parameters, insert
+                # padding if necessary for alignment.
+                for p in variable_params:
+                        if p.count_scale != 1:
+                                print ('   /* Next {0} bytes are '
+                                       '{1} {2}[{3}][{4}] */').format(
+                                        p.size_string(),
+                                        p.get_base_type_string(),
+                                        p.name, p.counter, p.count_scale)
+                        else:
+                                print ('   /* Next {0} bytes are '
+                                       '{1} {2}[{3}] */').format(
+                                        p.size_string(),
+                                        p.get_base_type_string(),
+                                        p.name, p.counter)
                 print '};'
                 print 'static inline void'
                 print ('unmarshal_{0}(struct gl_context *ctx, '
                        'const struct marshal_cmd_{0} *cmd)').format(func.name)
                 print '{'
-                print '   CALL_{0}(ctx->CurrentServerDispatch, ({1}));'.format(
-                        func.name,
-                        ', '.join('cmd->{0}'.format(p.name)
-                                  for p in func.parameters))
+                for p in fixed_params:
+                        if p.count:
+                                print '   const {0} * {1} = cmd->{1};'.format(
+                                        p.get_base_type_string(),
+                                        p.name)
+                        else:
+                                print '   const {0} {1} = cmd->{1};'.format(
+                                        p.type_string(), p.name)
+                if variable_params:
+                        for p in variable_params:
+                                print '   const {0} * {1};'.format(
+                                        p.get_base_type_string(),
+                                        p.name)
+                        print ('   const char *variable_data = '
+                               '(const char *) (cmd + 1);')
+                        for p in variable_params:
+                                print ('   {0} = (const {1} *) '
+                                       'variable_data;').format(
+                                        p.name, p.get_base_type_string())
+                                print '   variable_data += {0};'.format(
+                                        p.size_string(False))
+                self.print_sync_call(func, '   ')
                 print '}'
                 print 'static void GLAPIENTRY'
                 print 'marshal_{0}({1})'.format(
@@ -132,15 +217,21 @@ class PrintCode(gl_XML.gl_print_base):
                 print '{'
                 print '   GET_CURRENT_CONTEXT(ctx);'
                 struct = 'struct marshal_cmd_{0}'.format(func.name)
-                print '   {0} *cmd ='.format(struct)
-                print ('      _mesa_allocate_command_in_queue(ctx, '
-                       'DISPATCH_CMD_{0}, sizeof({1})').format(
-                        func.name, struct)
-                for p in params:
-                        print '   cmd->{0} = {0};'.format(p.name)
-                if not params:
-                        print '   (void) cmd;\n'
-                print '   _mesa_post_marshal_hook(ctx);'
+                size_terms = ['sizeof({0})'.format(struct)]
+                for p in variable_params:
+                        size_terms.append(p.size_string())
+                print '   size_t cmd_size = {0};'.format(
+                        ' + '.join(size_terms))
+                if variable_params:
+                        print '   if (cmd_size <= MARSHAL_MAX_CMD_SIZE) {'
+                        self.print_async_dispatch(func, struct, fixed_params,
+                                                  variable_params, '      ')
+                        print '   } else {'
+                        self.print_sync_dispatch(func, '      ')
+                        print '   }'
+                else:
+                        self.print_async_dispatch(func, struct, fixed_params,
+                                                  variable_params, '   ')
                 print '}'
                 print ''
                 print ''
@@ -204,5 +295,5 @@ if __name__ == '__main__':
 
 	printer = PrintCode()
 
-	api = gl_XML.parse_GL_API(file_name, glX_XML.glx_item_factory())
+	api = gl_XML.parse_GL_API(file_name)
 	printer.Print(api)
