85f2d566324834c8b90dabafa31ac4522ef2a1b0
Jordan Justen <jordan.l.justen@intel.com>
2012-10-29 10:37:50 -0700

    mesa: disable ProgramParameteri until ARB_geometry_shader4 or GLES3 is supported.  TODO: squash.
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    
    v3 (Paul Berry <stereotype441@gmail.com): update "exec" property in
    src/mapi/glapi/gen XML to reflect the change.

--- src/mesa/main/shaderapi.c
+++ src/mesa/main/shaderapi.c
@@ -1734,10 +1734,6 @@ _mesa_init_shader_dispatch(const struct gl_context *ctx,
       SET_GetAttribLocationARB(exec, _mesa_GetAttribLocationARB);
    }
 
-   if (_mesa_is_desktop_gl(ctx) || _mesa_is_gles3(ctx)) {
-      SET_ProgramParameteri(exec, _mesa_ProgramParameteriARB);
-   }
-
    if (_mesa_is_desktop_gl(ctx)) {
       SET_UseShaderProgramEXT(exec, _mesa_UseShaderProgramEXT);
       SET_ActiveProgramEXT(exec, _mesa_ActiveProgramEXT);
