8893f671a8be177accab2718ae72ba0e75ac16da
Paul Berry <stereotype441@gmail.com>
2013-03-22 12:34:19 -0700

    i965/gs: make the state atom for compiling Gen7 geometry shaders.

--- src/mesa/drivers/dri/i965/brw_context.h
+++ src/mesa/drivers/dri/i965/brw_context.h
@@ -693,6 +693,7 @@ enum brw_cache_id {
    BRW_VS_PROG,
    BRW_GS_UNIT,
    BRW_GS_PROG,
+   BRW_VEC4_GS_PROG,
    BRW_CLIP_VP,
    BRW_CLIP_UNIT,
    BRW_CLIP_PROG,
@@ -783,6 +784,7 @@ enum shader_time_shader_type {
 #define CACHE_NEW_VS_PROG                (1<<BRW_VS_PROG)
 #define CACHE_NEW_GS_UNIT                (1<<BRW_GS_UNIT)
 #define CACHE_NEW_GS_PROG                (1<<BRW_GS_PROG)
+#define CACHE_NEW_VEC4_GS_PROG           (1<<BRW_VEC4_GS_PROG)
 #define CACHE_NEW_CLIP_VP                (1<<BRW_CLIP_VP)
 #define CACHE_NEW_CLIP_UNIT              (1<<BRW_CLIP_UNIT)
 #define CACHE_NEW_CLIP_PROG              (1<<BRW_CLIP_PROG)
