4f8bdf7347814c975bc8fddfcfc814ee9d0b3453
Paul Berry <stereotype441@gmail.com>
2013-03-23 07:59:13 -0700

    Add GS_OPCODE_SET_WRITE_OFFSET

--- src/mesa/drivers/dri/i965/brw_vec4_emit.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_emit.cpp
@@ -444,6 +444,20 @@ vec4_generator::generate_gs_thread_end(vec4_instruction *inst)
 }
 
 void
+vec4_generator::generate_gs_set_write_offset(struct brw_reg dst,
+                                             struct brw_reg src0,
+                                             struct brw_reg src1)
+{
+   brw_push_insn_state(p);
+   brw_set_access_mode(p, BRW_ALIGN_1);
+   brw_set_mask_control(p, BRW_MASK_DISABLE);
+   brw_MUL(p, suboffset(stride(dst, 2, 2, 1), 3), stride(src0, 8, 2, 4),
+           src1);
+   brw_set_access_mode(p, BRW_ALIGN_16);
+   brw_pop_insn_state(p);
+}
+
+void
 vec4_generator::generate_oword_dual_block_offsets(struct brw_reg m1,
                                                   struct brw_reg index)
 {
@@ -730,6 +744,10 @@ vec4_generator::generate_vec4_instruction(vec4_instruction *instruction,
 
    case GS_OPCODE_THREAD_END:
       generate_gs_thread_end(inst);
+      break;
+
+   case GS_OPCODE_SET_WRITE_OFFSET:
+      generate_gs_set_write_offset(dst, src[0], src[1]);
       break;
 
    case SHADER_OPCODE_SHADER_TIME_ADD:
