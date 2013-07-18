mm120/mesa-anholt

!UPSTREAM: Expose GLSL 1.50 and GL 3.2 core contexts.
This lets me run tests normally, since globally doing GL version override
can break tests that require a compatibility context.

src/mesa/drivers/dri/intel/intel_extensions.c
@@ -85,7 +85,9 @@ intelInitExtensions(struct gl_context *ctx)
    ctx->Extensions.OES_EGL_image = true;
    ctx->Extensions.OES_draw_texture = true;
 
-   if (intel->gen >= 6)
+   if (intel->gen >= 7)
+      ctx->Const.GLSLVersion = 150;
+   else if (intel->gen >= 6)
       ctx->Const.GLSLVersion = 140;
    else
       ctx->Const.GLSLVersion = 120;

stereotype441/mesa

src/mesa/drivers/dri/i965/intel_extensions.c
@@ -123,7 +123,9 @@ intelInitExtensions(struct gl_context *ctx)
    ctx->Extensions.OES_draw_texture = true;
    ctx->Extensions.OES_standard_derivatives = true;
 
-   if (brw->gen >= 6)
+   if (brw->gen >= 7)
+      ctx->Const.GLSLVersion = 150;
+   else if (brw->gen >= 6)
       ctx->Const.GLSLVersion = 140;
    else
       ctx->Const.GLSLVersion = 120;