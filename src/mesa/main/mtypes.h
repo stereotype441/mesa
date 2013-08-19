8d95373188405071a115e94c1283eab6b5c9db7f
Paul Berry <stereotype441@gmail.com>
2013-08-18 20:59:37 -0700

    glsl: During linking, record whether a GS uses EndPrimitive().
    This information will be useful in the i965 back end, since we can
    save some compilation effort if we know from the outset that the
    shader never calls EndPrimitive().

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -1925,6 +1925,7 @@ struct gl_geometry_program
                            GL_TRIANGLES, or GL_TRIANGLES_ADJACENCY_ARB */
    GLenum OutputType; /**< GL_POINTS, GL_LINE_STRIP or GL_TRIANGLE_STRIP */
    GLboolean UsesClipDistance;
+   GLboolean UsesEndPrimitive;
 };
 
 
@@ -2356,6 +2357,7 @@ struct gl_shader_program
       GLboolean UsesClipDistance;
       GLuint ClipDistanceArraySize; /**< Size of the gl_ClipDistance array, or
                                          0 if not present. */
+      GLboolean UsesEndPrimitive;
    } Geom;
 
    /** Vertex shader state */
