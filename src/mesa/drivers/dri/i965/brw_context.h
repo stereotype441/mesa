ffe534f377baf38e0c91e5a5c13013b190c512e7
Paul Berry <stereotype441@gmail.com>
2013-08-26 22:25:26 -0700

    !UPSTREAM: i965/gs: Fix gl_PrimitiveIDIn when using SW primitive restart.
    TODO: We really ought to set _NEW_PROGRAM_CONSTANTS only when software
    primitive restart is in use.  Currently we set it for hardware
    primitive restart too.

--- src/mesa/drivers/dri/i965/brw_context.h
+++ src/mesa/drivers/dri/i965/brw_context.h
@@ -591,6 +591,7 @@ struct brw_gs_prog_data
    unsigned control_data_format;
 
    bool include_primitive_id;
+   bool need_primitive_id_workaround;
 };
 
 /** Number of texture sampler units */
@@ -1365,6 +1366,7 @@ struct brw_context
    struct {
       bool in_progress;
       bool enable_cut_index;
+      GLuint sw_prim_counter;
    } prim_restart;
 
    /** Computed depth/stencil/hiz state from the current attached
